bac6a4d2b01434ffbbb0df25d45930e125cef813
Deep Surati <deepsurati02@gmail.com>
2025-03-08 22:31:54 +0530

    feat: integrate Clerk auth in Navbar

---
+++ frontend/app/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+const Navbar = () => {
+  return (
+    <div className='bg-red-600 flex justify-between items-center w-full h-11 px-3 py-1'>
+      <div className='text-2xl text-center text-black border-e-violet-200 font-medium'>
+        DeepWrite AI
+      </div>
+      <div>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
